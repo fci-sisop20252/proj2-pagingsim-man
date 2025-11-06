d2af13aa4b152af1ce6eb190d265b558024c2ff2
Matheus Kroeff <matheuslckroeff@gmail.com>
2025-11-06 21:15:57 +0000

    Criei uma struct para os processos e uma para os Frames, tirando os ponteiros de dentro da struct da RAM

--- include/ram.h
+++ include/ram.h
@@ -4,48 +4,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/**
- * @param frames Lista de frames, que guarda o ID do PID que está em cada frame. -1 se estiver vazio.
- * @param num_frames Quantidade de frames disponível.
- * @param ram_pag Tamanho de cada frame.
- *
- * @param processos Lista dos PIDs no seguinte formato: [<id pid>][<tamanho da memória nescessária>].
- * @param num_processos Quantidade de processos.
- */
+typedef struct Processo {
+    int pid;
+    int tamanho_memoria_virtual;
+    int num_paginas;
+    PageTableEntry* tabela_paginas;
+} Processo;
+
 typedef struct RAM {
-    int* frames;
+    Frame* frames;
     int num_frames;
     int tam_pag;
-    int** processos;
+
+    Processo* processos;
     int num_processos;
+
+    int clock_pointer;
+    int next_fifo_index;
+
+    int total_page_faults;
+    int total_acessos;
+
 } RAM;
 
-/**
- * @brief Cria nova estrutura de memória RAM e processos
- * 
- * @return @c RAM Estrutura RAM preenchida
- */
+typedef struct Frame {
+    int pid;
+    int pagina_virtual;
+    int r_bit;
+
+    int ordem_chegada;
+} Frame;
+
+
 RAM criarRAM(int num_frames, int tam_pag, int** processos, int num_processos);
 
-/**
- * @brief Exclui memória alocada de RAM
- * 
- * @param ram  @c RAM a ser desalocada
- */
 void freeRAM(RAM ram);
 
-/**
- * @brief Printa os PIDs de cada Frame
- * 
- * @param ram  @c RAM a ser printada
- */
 void printFrames(RAM ram);
 
-/**
- * @brief Printa os processos da RAM
- * 
- * @param ram  @c RAM a ser printada
- */
 void printProcessos(RAM ram);
 
 #endif // RAM_H
